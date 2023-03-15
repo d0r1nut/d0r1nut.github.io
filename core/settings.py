90c2f6137454afa5486ee1545695a82f90624775
Dmytro <dimaland2k17@gmail.com>
2023-03-15 02:58:07 -0400

    updated

--- core/settings.py
+++ core/settings.py
@@ -125,8 +125,6 @@ STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATIC_URL = '/static/'
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
-MEDIA_URL = '/images/'
-MEDIA_ROOT = BASE_DIR / 'static/images'
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
