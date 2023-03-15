3a65a77e0bbc86872a9add0188aaf74511524525
Dmytro <dimaland2k17@gmail.com>
2023-03-15 11:00:50 -0400

    static fixing v4

--- core/settings.py
+++ core/settings.py
@@ -121,7 +121,7 @@ USE_TZ = True
 
 STATIC_ROOT = os.path.join(BASE_DIR, "static")
 STATIC_URL = '/static/'
-STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
+STATICFILES_DIRS = os.path.join(BASE_DIR, 'staticfiles'),
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # Default primary key field type
